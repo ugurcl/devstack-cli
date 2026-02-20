afb4f45f20f369a2323413a16d2194eac515d018
Uğur Çalışkan <infougurcalskan@gmail.com>
2026-02-21 02:01:44 +0300

    add rate limit generator

---
+++ src/generators/rate-limit.gen.ts
@@ -0,0 +1,19 @@
+import { FileOutput } from "../types/index.js";
+
+export function generateRateLimit(): FileOutput[] {
+  return [
+    {
+      path: "src/middlewares/rateLimiter.middleware.ts",
+      content: `import rateLimit from "express-rate-limit";
+
+export const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: { status: "error", message: "Too many requests, try again later" },
+});
+`,
+    },
+  ];
+}
